2076f80cd38ffe323c8f20240cc98c22c84c7c4b
jayfir <chinhui@coderfarmer.com>
2018-05-23 15:37:11 +0800

    1

--- common/models/wechat/FansStat.php
+++ common/models/wechat/FansStat.php
@@ -1,24 +1,36 @@
 <?php
-
 namespace jayfir\basics\common\models\wechat;
 
 use Yii;
-use EasyWeChat\Factory;
+use yii\db\ActiveRecord;
+use yii\behaviors\TimestampBehavior;
 
 /**
- * This is the model class for table "{{%wechat_fans_groups}}".
+ * This is the model class for table "{{%wechat_fans_stat}}".
  *
- * @property integer $id
- * @property string $tags
+ * @property string $id
+ * @property string $new_attention
+ * @property string $cancel_attention
+ * @property integer $cumulate_attention
+ * @property string $date
  */
-class FansTags extends \yii\db\ActiveRecord
+class FansStat extends \yii\db\ActiveRecord
 {
+    /**
+     * 新关注
+     */
+    const NEW_ATTENTION = 1;
+    /**
+     * 取消关注
+     */
+    const CANCEL_ATTENTION = 2;
+
     /**
      * @inheritdoc
      */
     public static function tableName()
     {
-        return '{{%wechat_fans_tags}}';
+        return '{{%wechat_fans_stat}}';
     }
 
     /**
@@ -27,7 +39,9 @@ class FansTags extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['tags'], 'string'],
+            [['new_attention', 'cancel_attention', 'cumulate_attention'], 'integer'],
+            [['date'], 'required'],
+            [['date'], 'safe'],
         ];
     }
 
@@ -37,74 +51,188 @@ class FansTags extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'id'      => 'ID',
-            'tags'    => '标签',
+            'id' => 'ID',
+            'new_attention' => '今日新关注',
+            'cancel_attention' => '今日取消关注',
+            'cumulate_attention' => '累计关注',
+            'date' => '日期',
         ];
     }
 
     /**
-     * 获取标签信息
-     * @return mixed
+     * 类别 1 关注 2 取消关注
+     * @param $type
      */
-    public static function getTags($app)
+    public static function addStat($type)
     {
-        if (empty(($model = self::find()->one())))
-        {
-            $list = $app->user_tag->list();
+        $date = date('Y-m-d');
 
+        $model = self::findOne(['date' => $date]);
+        if(!$model)
+        {
+            $model = new self;
+            $model->loadDefaultValues();
+            $model->date = $date;
+        }
+
+        $type == 1 ? $model->new_attention +=1 : $model->cancel_attention +=1;
+
+        return $model->save();
+    }
+
+    /**
+     * 关注计算
+     */
+    public static function upFollowNum()
+    {
+        if(!($today = self::find()->where(['date' => date('Y-m-d')])->one()))
+        {
+            $today = new self();
+            $today->date = date('Y-m-d');
+            $today->append = strtotime($today->date);
+        }
+
+        $today->new_attention += 1;
+        $today->save();
+    }
+
+    /**
+     * 取消关注计算
+     */
+    public static function upUnFollowNum()
+    {
+        if(!($today = self::find()->where(['date' => date('Y-m-d')])->one()))
+        {
+            $today = new self();
+            $today->date = date('Y-m-d');
+            $today->append = strtotime($today->date);
+        }
+
+        $today->cancel_attention += 1;
+        $today->save();
+    }
+
+    /**
+     * @param $app
+     * @return bool
+     */
+    public static function getFansStat($app)
+    {
+        // 缓存设置
+        $cacheKey = 'fans:status:todaylock';
+        if(Yii::$app->cache->get($cacheKey))
+        {
+            return true;
+        }
+
+        $sevenDays = [
+            date('Y-m-d', strtotime('-1 days')),
+            date('Y-m-d', strtotime('-2 days')),
+            date('Y-m-d', strtotime('-3 days')),
+            date('Y-m-d', strtotime('-4 days')),
+            date('Y-m-d', strtotime('-5 days')),
+            date('Y-m-d', strtotime('-6 days')),
+            date('Y-m-d', strtotime('-7 days')),
+        ];
+
+        $models = self::find()
+            ->where(['in','date',$sevenDays])
+            ->all();
+
+        $statUpdate = false;
+        $weekStat = [];
+        foreach ($models as $model)
+        {
+            $weekStat[$model['date']] = $model;
+        }
+
+        // 查询数据是否有
+        foreach ($sevenDays as $sevenDay)
+        {
+            if (empty($weekStat[$sevenDay]) || $weekStat[$sevenDay]['cumulate_attention'] <= 0)
+            {
+                $statUpdate = true;
+                break;
+            }
+        }
+
+        if (empty($statUpdate))
+        {
+            return true;
+        }
+
+        // 获取微信统计数据
+        $stats = $app->data_cube;
+        // 增减
+        $userSummary = $stats->userSummary($sevenDays[6], $sevenDays[0]);
+        // 累计用户
+        $userCumulate = $stats->userCumulate($sevenDays[6], $sevenDays[0]);
+
+        $list = [];
+        if (!empty($userSummary['list']))
+        {
+            foreach ($userSummary['list'] as $row)
+            {
+                $key = $row['ref_date'];
+                $list[$key]['new_attention'] = $row['new_user'];
+                $list[$key]['cancel_attention'] = $row['cancel_user'];
+            }
+        }
+
+        if (!empty($userCumulate['list']))
+        {
+            foreach ($userCumulate['list'] as $row)
+            {
+                $key = $row['ref_date'];
+                $list[$key]['cumulate_attention'] = $row['cumulate_user'];
+            }
+        }
+
+        // 更新到数据库
+        foreach ($list as $key => $value)
+        {
             $model = new self();
-            $model->tags = serialize($list['tags']);
+            if(isset($weekStat[$key]))
+            {
+                $model = $weekStat[$key];
+            }
+
+            $model->attributes = $value;
+            $model->date = $key;
+            $model->append = strtotime($key);
             $model->save();
         }
 
-        return unserialize($model->tags);
-    }
-
-    /**
-     * 获取单个标签信息
-     * @param $id
-     * @return array|mixed
-     */
-    public static function getTag($id)
-    {
-        $model = self::find()->one();
-        $tags = unserialize($model->tags);
-        foreach ($tags as $vo)
+        // 今日累计关注统计计算
+        $cumulate_attention = Fans::getCountFollowFans();
+        if(!($today = self::find()->where(['date' => date('Y-m-d')])->one()))
         {
-            if($vo['id'] == $id)
-            {
-                return $vo;
-            }
-        }
-    }
-
-    /**
-     * 获取标签信息并保存到数据库
-     */
-    public static function updateTagsList($app)
-    {
-        $list = $app->user_tag->list();
-
-        $tags = $list['tags'];
-        if (empty(($model = FansTags::find()->one())))
-        {
-            $model = new self();
+            $today = new self();
+            $today->date = date('Y-m-d');
+            $today->append = strtotime($today->date);
         }
 
-        $model->tags = serialize($tags);
-        $model->save();
+        $today->cumulate_attention = $cumulate_attention;
+        $today->save();
 
-        return $tags;
+        Yii::$app->cache->set($cacheKey,true,7200);
+        return true;
     }
 
     /**
-     * 删除粉丝关联标签
-     *
-     * @return bool
+     * 行为
+     * @return array
      */
-    public function beforeDelete()
+    public function behaviors()
     {
-        FansTagMap::deleteAll(['tag_id' => $this->id]);
-        return parent::beforeDelete();
+        return [
+            [
+                'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['updated'],
+                ],
+            ],
+        ];
     }
+
 }
